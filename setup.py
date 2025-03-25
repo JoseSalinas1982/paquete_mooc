1400fcd2cc0da6da31e9b0e5a4a66ab393c9c395
JoseSalinas1982 <143436633+JoseSalinas1982@users.noreply.github.com>
2025-03-25 17:57:10 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paquete_mooc',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Descripción breve del paquete',
+    author='J. Salinas',
+    author_email='javier.salinas1982@gmail.com',
+    url='URL del repositorio de GitHub',
+    install_requires=[
+        # Lista de dependencias necesarias
+    ],
+)
